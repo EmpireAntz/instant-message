0bf50d3feea0d721beaacb9b4b9e061d1f1e499b
EmpireAntz <thebigboognish@gmail.com>
2023-12-21 20:39:17 -0800

    starting pseudo code and wireframe

---
+++ pseudo.js
@@ -0,0 +1,8 @@
+// Login/Sign in page
+// Auth for login should use email since they are inherently unique
+// Search and add friends via email
+// Create group messages or directly message people
+// Messages between users persist
+// Editable messages
+// Messages can be deleted 
+// Users can delete friends
