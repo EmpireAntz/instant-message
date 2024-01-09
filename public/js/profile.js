3f8dead07f219fd59e7890fb697a160b30c4b67f
Dylan Coito <dylanmatthewcoito@gmail.com>
2024-01-08 21:09:25 -0800

    Added styling and profile avatar icon

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,17 +1,13 @@
-// Code to display a message when a card is clicked
-function showMessage(message) {
-  alert(message);
+// Functionality for changing profile avatar bear 
+var imageIndex = 0;
+var images = ["/images/purple-bear.jpg", "/images/pink-bear.jpg", "/images/green-bear.jpg", "/images/blue-bear.jpg"];
+
+document.getElementById("currentImage").addEventListener("click", changeImage);
+
+function changeImage() {
+  var image = document.getElementById("currentImage");
+  imageIndex = (imageIndex + 1) % images.length;
+  
+  image.src = images[imageIndex];
 }
 
-// Code to add click event listener to all the cards
-document.addEventListener("DOMContentLoaded", function() {
-  var cards = document.querySelectorAll(".card");
-  
-  // Add click event listener to each card
-  cards.forEach(function(card) {
-    card.addEventListener("click", function() {
-      var message = card.querySelector(".card-text").textContent;
-      showMessage(message);
-    });
-  });
-});
