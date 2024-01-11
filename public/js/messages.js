1eb2da491c58bdeb3ded1ed8a93712a6c0c5c480
EmpireAntz <thebigboognish@gmail.com>
2024-01-11 14:34:30 -0800

    added socket to the server, server side console will show when a user connects and disconnects

--- public/js/messages.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("submitMessage").addEventListener("click", function () {
-    var message = document.getElementById("messageInput").value;
-    // document.getElementById("messageBoard").innerHTML += "<div class='message-card'><p class='message-content'>" + message + "</p></div>";
-    document.getElementById("messageInput").value = "";
-
-});
-
